826bc04ec36c875e06f80967af6de5dd338db1a9
Jay Jackson <jj@jj.is-a.dev>
2022-04-25 05:30:31 +0000

    hmm

--- src/config.rs
+++
@@ -1,9 +0,0 @@
-pub struct Config {
-    pub download_quality: DownloadQuality,
-}
-
-pub enum DownloadQuality {
-    Normal,
-    High,
-    VeryHigh,
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,23 @@
-pub mod config;
+use std::io::Read;
 
 use librespot::{
+    audio::{AudioDecrypt, AudioFile},
     core::{
+        audio_key::AudioKeyError,
+        channel::ChannelError,
         config::SessionConfig,
-        keymaster::{get_token, Token},
         mercury::MercuryError,
         session::{Session, SessionError},
+        spotify_id::{FileId, SpotifyId},
     },
     discovery::Credentials,
+    metadata::{AudioItem, FileFormat},
 };
 
 use thiserror::Error;
 
 pub struct Beater {
-    session: Session,
-    token: Token,
-    pub config: config::Config,
+    pub(crate) session: Session,
 }
 
 #[derive(Debug, Error)]
@@ -24,6 +26,16 @@ pub enum Error {
     SessionError(#[from] SessionError),
     #[error("An unknown error given from a Murcury request")]
     MurcuryError,
+    #[error("An unknown error given from a Channel request")]
+    ChannelError,
+    #[error("An `Option` ({0}) was None")]
+    NoneOption(&'static str),
+    #[error(
+        "A file with the {0:?} file format was not found, the available file formats are {1:?}"
+    )]
+    FileFormatNotFound(FileFormat, Vec<FileFormat>),
+    #[error("An unknown error given when requesting an `AudioKey`")]
+    AudioKeyError,
 }
 
 impl From<MercuryError> for Error {
@@ -31,15 +43,25 @@ impl From<MercuryError> for Error {
         Self::MurcuryError
     }
 }
+impl From<ChannelError> for Error {
+    fn from(_: ChannelError) -> Self {
+        Self::ChannelError
+    }
+}
+impl From<AudioKeyError> for Error {
+    fn from(_: AudioKeyError) -> Self {
+        Self::AudioKeyError
+    }
+}
 
 pub(crate) type Result<T> = std::result::Result<T, Error>;
 
 impl Beater {
-    pub async fn new(
-        username: impl Into<String>,
-        password: impl Into<String>,
-        config: config::Config,
-    ) -> Result<Self> {
+    /// Creates a new [`Beater`]
+    ///
+    /// This connects with your Spotify account, and creates a token that can read your
+    /// liked songs, private playlists, and your email
+    pub async fn new(username: impl Into<String>, password: impl Into<String>) -> Result<Self> {
         let session = Session::connect(
             SessionConfig {
                 user_agent: concat!(
@@ -48,49 +70,99 @@ impl Beater {
                     "Chrome/100.0.4896.127 Safari/537.36"
                 )
                 .to_owned(),
-                device_id: "0".to_owned(),
+                device_id: String::new(),
                 proxy: None,
                 ap_port: None,
             },
             Credentials::with_password(username, password),
             None,
         )
-        .await
-        .unwrap();
-
-        let token = get_token(
-            &session,
-            "b8682ddfdcd04c31a14ebd926a7e7f07",
-            "user-read-email,playlist-read-private,user-library-read",
-        )
         .await?;
 
-        Ok(Self {
-            session,
-            token,
-            config,
-        })
+        Ok(Self { session })
     }
+
+    pub async fn get_song(&self, id: SpotifyId) -> Result<AudioItem> {
+        AudioItem::get_audio_item(&self.session, id)
+            .await
+            .map_err(|e| Error::from(e))
+    }
+
+    pub async fn get_audio_file(
+        &self,
+        audio_item: AudioItem,
+        music_format: FileFormat,
+    ) -> Result<AudioFile> {
+        match audio_item
+            .files
+            .get(&music_format)
+            .map(|file_id| file_id.clone())
+        {
+            Some(file_id) => {
+                AudioFile::open(&self.session, file_id, 40 * 1024, true)
+                    .await
+                    .map_err(|e| Error::from(e))
+
+                // self.decrypt_audio_file(audio_item.id, file_id, audio_file)
+                //     .await
+            }
+            None => Err(Error::FileFormatNotFound(
+                music_format,
+                audio_item.files.into_keys().collect(),
+            )),
+        }
+    }
+
+    // pub(crate) async fn decrypt_audio_file<T: Read>(
+    //     &self,
+    //     spotify_id: SpotifyId,
+    //     file_id: FileId,
+    //     audio_file: T,
+    // ) -> Result<AudioDecrypt<T>> {
+    //     let key = self
+    //         .session
+    //         .audio_key()
+    //         .request(spotify_id, file_id)
+    //         .await?;
+
+    //     Ok(AudioDecrypt::new(key, audio_file))
+    // }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::io::Read;
+
     use crate::*;
 
     async fn create() -> Beater {
-        Beater::new(
-            "31woy7dllvxal6lcroelpl5s2rhu",
-            "idrcwhattoputasapassw0rd",
-            config::Config {
-                download_quality: config::DownloadQuality::High,
-            },
-        )
-        .await
-        .unwrap()
+        simplelog::SimpleLogger::init(log::LevelFilter::Debug, simplelog::Config::default()).ok();
+
+        Beater::new("31woy7dllvxal6lcroelpl5s2rhu", "idrcwhattoputasapassw0rd")
+            .await
+            .unwrap()
     }
 
     #[tokio::test]
     async fn new_beater() {
         create().await;
+    }
+
+    #[tokio::test]
+    async fn get_audio_file() {
+        let beater = create().await;
+        let song = beater
+            .get_song(SpotifyId::from_base62("2QTDuJIGKUjR7E2Q6KupIh").unwrap())
+            .await
+            .unwrap();
+        let mut file = beater
+            .get_audio_file(song, FileFormat::OGG_VORBIS_320)
+            .await
+            .unwrap();
+
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).unwrap();
+
+        std::fs::write("song.ogg", buf).unwrap();
     }
 }
