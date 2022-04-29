df0aa318c4dfe2624021d5c717f1a916730db43c
Jay Jackson <jj@jj.is-a.dev>
2022-04-28 21:19:34 -0400

    I have an idea

--- src/lib.rs
+++ src/lib.rs
@@ -6,60 +6,17 @@
 use librespot::{
     audio::{AudioDecrypt, AudioFile},
     core::{
-        audio_key::AudioKeyError,
-        channel::ChannelError,
         config::SessionConfig,
-        mercury::MercuryError,
-        session::{Session, SessionError},
+        error::Error,
+        session::Session,
         spotify_id::{FileId, SpotifyId},
     },
     discovery::Credentials,
-    metadata::AudioItem,
+    metadata::audio::{AudioFileFormat, AudioItem},
 };
-
-use thiserror::Error;
 
 pub struct Beater {
     pub(crate) session: Session,
-}
-
-pub use librespot::metadata::FileFormat;
-
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error(transparent)]
-    SessionError(#[from] SessionError),
-    #[error("An unknown error given from a Murcury request")]
-    MurcuryError,
-    #[error("An unknown error given from a Channel request")]
-    ChannelError,
-    #[error("An `Option` ({0}) was None")]
-    NoneOption(&'static str),
-    #[error(
-        "A file with the {0:?} file format was not found, the available file formats are {1:?}"
-    )]
-    FileFormatNotFound(FileFormat, Vec<FileFormat>),
-    #[error(
-        "An unknown error given when requesting an `AudioKey`. {}",
-        "This usually means that you are requesting a file quality that is too high for your account"
-    )]
-    AudioKeyError,
-}
-
-impl From<MercuryError> for Error {
-    fn from(_: MercuryError) -> Self {
-        Self::MurcuryError
-    }
-}
-impl From<ChannelError> for Error {
-    fn from(_: ChannelError) -> Self {
-        Self::ChannelError
-    }
-}
-impl From<AudioKeyError> for Error {
-    fn from(_: AudioKeyError) -> Self {
-        Self::AudioKeyError
-    }
 }
 
 pub(crate) type Result<T> = std::result::Result<T, Error>;
@@ -84,28 +41,15 @@ impl Beater {
     ///
     /// This function will return an error if the [`Session`] fails to connect (e.g. your credentials are wrong).
     pub async fn new(username: impl Into<String>, password: impl Into<String>) -> Result<Self> {
-        Ok(Self {
-            session: Session::connect(
-                SessionConfig {
-                    user_agent: concat!(
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ",
-                        "AppleWebKit/537.36 (KHTML, like Gecko) ",
-                        "Chrome/100.0.4896.127 Safari/537.36"
-                    )
-                    .to_owned(),
-                    device_id: String::new(),
-                    proxy: None,
-                    ap_port: None,
-                },
-                Credentials::with_password(username, password),
-                None,
-            )
-            .await?,
-        })
+        let session = Session::new(SessionConfig::default(), None);
+        session
+            .connect(Credentials::with_password(username, password))
+            .await?;
+        Ok(Self { session })
     }
 
     pub async fn get_audio_item(&self, id: SpotifyId) -> Result<AudioItem> {
-        AudioItem::get_audio_item(&self.session, id)
+        AudioItem::get_file(&self.session, id)
             .await
             .map_err(Error::from)
     }
@@ -113,25 +57,19 @@ impl Beater {
     pub async fn get_audio_file(
         &self,
         audio_item: &AudioItem,
-        music_format: FileFormat,
+        music_format: AudioFileFormat,
     ) -> Result<(AudioDecrypt<AudioFile>, FileId)> {
         if let Some(file_id) = audio_item.files.get(&music_format).copied() {
-            let encrypted = AudioFile::open(&self.session, file_id, 1024 * 1024, true)
-                .await
-                .map_err(Error::from)?;
-            encrypted.get_stream_loader_controller().set_stream_mode();
+            let encrypted = AudioFile::open(&self.session, file_id, 1024 * 1024).await?;
+            encrypted.get_stream_loader_controller()?.set_stream_mode();
 
             let decrypted = self
                 .decrypt_audio_file(audio_item.id, file_id, encrypted)
-                .await
-                .ok_or(Error::AudioKeyError)?;
+                .await?;
 
             Ok((decrypted, file_id))
         } else {
-            Err(Error::FileFormatNotFound(
-                music_format,
-                audio_item.files.keys().cloned().collect(),
-            ))
+            Err(Error::not_found(""))
         }
     }
 
@@ -140,14 +78,13 @@ impl Beater {
         spotify_id: SpotifyId,
         file_id: FileId,
         audio_file: AudioFile,
-    ) -> Option<AudioDecrypt<AudioFile>> {
+    ) -> Result<AudioDecrypt<AudioFile>> {
         let key = self
             .session
             .audio_key()
             .request(spotify_id, file_id)
-            .await
-            .ok()?;
-        Some(AudioDecrypt::new(key, audio_file))
+            .await?;
+        Ok(AudioDecrypt::new(Some(key), audio_file))
     }
 
     pub fn session(&self) -> &Session {
@@ -162,6 +99,7 @@ impl Beater {
 #[cfg(test)]
 mod tests {
     use std::{
+        env,
         fs::OpenOptions,
         io::{Read, Seek, SeekFrom, Write},
     };
@@ -171,9 +109,13 @@ mod tests {
     async fn create() -> Beater {
         simplelog::SimpleLogger::init(log::LevelFilter::Debug, simplelog::Config::default()).ok();
 
-        Beater::new("31woy7dllvxal6lcroelpl5s2rhu", "idrcwhattoputasapassw0rd")
-            .await
-            .unwrap()
+        Beater::new(
+            env::var("SPOTIFY_USERNAME")
+                .unwrap_or_else(|_| "31woy7dllvxal6lcroelpl5s2rhu".to_owned()),
+            env::var("SPOTIFY_PASSWORD").unwrap_or_else(|_| "idrcwhattoputasapassw0rd".to_owned()),
+        )
+        .await
+        .unwrap()
     }
 
     #[tokio::test]
@@ -190,7 +132,7 @@ mod tests {
         let song = beater.get_audio_item(spotify_id).await.unwrap();
 
         let (mut audio_file, _file_id) = beater
-            .get_audio_file(&song, FileFormat::OGG_VORBIS_160)
+            .get_audio_file(&song, AudioFileFormat::OGG_VORBIS_160)
             .await
             .unwrap();
 
