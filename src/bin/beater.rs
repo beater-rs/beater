41d3dfaa187bc1e635364a50813e650c58ff609a
Jay Jackson <jj@jj.is-a.dev>
2022-05-24 14:59:48 -0400

    fix ci x3

--- src/bin/beater.rs
+++ src/bin/beater.rs
@@ -1,12 +1,10 @@
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf, io, error};
+use std::{error, fs, io, path::PathBuf};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[clap(short, long)]
     username: Option<String>,
     #[clap(short, long)]
