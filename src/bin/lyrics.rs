eaa86db017612b5f8ff0d52702bebce27d761a35
Jay Jackson <jj@jj.is-a.dev>
2022-05-31 22:07:20 -0400

    update lyrics

--- src/bin/lyrics.rs
+++ src/bin/lyrics.rs
@@ -1,14 +1,12 @@
-use std::collections::HashMap;
-
 use librespot_core::{Session, SpotifyId};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Lyrics {
     pub lyrics: InnerLyrics,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd)]
 #[serde(rename_all = "camelCase")]
 pub struct InnerLyrics {
     pub provider: String,
@@ -17,15 +15,15 @@ pub struct InnerLyrics {
     pub lines: Vec<LyricsLine>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd)]
 pub struct LyricsLine {
     #[serde(rename = "words")]
-    pub text: Vec<InnerLyricsLine>,
+    pub text: Vec<LyricWord>,
     pub time: u32,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
-pub struct InnerLyricsLine {
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd)]
+pub struct LyricWord {
     #[serde(rename = "string")]
     pub text: String,
 }
@@ -45,13 +43,20 @@ impl Lyrics {
         .map_err(|e| Box::new(e) as _)
     }
 
-    pub async fn into_lrc_file(self) -> crate::Result<String> {
+    pub async fn into_lrc_file(self) -> String {
+        fn calculate_time(ms: u32) -> String {
+            let secs = ms / 1000;
+            let mins = secs / 60;
+            let secs = secs % 60;
+            format!("{:02}:{:02}", mins, secs)
+        }
+
         let mut lines = Vec::new();
         for line in self.lyrics.lines {
             for word in line.text {
-                lines.push(format!("[{}]{}", line.time, word.text));
+                lines.push(format!("[{}]{}", calculate_time(line.time), word.text));
             }
         }
-        Ok(lines.join("\n"))
+        lines.join("\n")
     }
 }
